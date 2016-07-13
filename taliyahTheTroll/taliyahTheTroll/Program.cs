68d61069153af05c3870ae65cb4fb77bc922d61c
FireBuddy <aijun@live.ca>
2016-07-13 18:41:09 -0400

    Update Program.cs

--- taliyahTheTroll/taliyahTheTroll/Program.cs
+++ taliyahTheTroll/taliyahTheTroll/Program.cs
@@ -76,8 +76,10 @@ namespace taliyahTheTroll
             }
             if (W.IsReady() && !sender.IsInvulnerable && args.Target != CurrentTarget && !sender.IsDashing() && sender == CurrentTarget)
             {
-
-
+               if (args.End.Distance(Player.Instance.Position) >= 100 || args.SData.TargettingType == SpellDataTargetType.Unit)
+                {
+                    
+                }
 
                 
 
