8f77fdb6af1ec3ae5c36d4881a0b11237869cf1f
FireBuddy <aijun@live.ca>
2016-07-13 19:20:05 -0400

    Update Program.cs

--- taliyahTheTroll/taliyahTheTroll/Program.cs
+++ taliyahTheTroll/taliyahTheTroll/Program.cs
@@ -25,8 +25,8 @@ namespace taliyahTheTroll
         public static bool Out = false;
         public static int CurrentSkin;
         public static AIHeroClient CurrentTarget;
-        public int LastCastTime;
-        public Vector3 LastEndPosition;
+        public int W.LastCastTime;
+        public Vector3 W.LastEndPosition;
 
      
 
