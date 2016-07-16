00c875cd011aa9f63b2c866a6d67cf7bc4029e6d
FireBuddy <aijun@live.ca>
2016-07-16 11:20:05 -0400

    Update Program.cs

--- taliyahTheTroll/taliyahTheTroll/Program.cs
+++ taliyahTheTroll/taliyahTheTroll/Program.cs
@@ -275,7 +275,7 @@ namespace taliyahTheTroll
             {
                 OnJungle();
             }
-            if (Orbwalker.ActiveModesFlags.HasFlag(Orbwalker.ActiveModes.Flee) && Q5x = true;)
+            if (Orbwalker.ActiveModesFlags.HasFlag(Orbwalker.ActiveModes.Flee) && Q5x = true)
             {
                 Q.Cast(Game.CursorPos);
             }
